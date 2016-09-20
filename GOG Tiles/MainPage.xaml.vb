9c9eb51a134c925bdce6cad7b043c0d2e67cabee
pepei <pepei@DESKTOP-4NFJ4L7>
2016-09-20 10:39:39 +0200

    Add project files.

---
+++ GOG Tiles/MainPage.xaml.vb
@@ -0,0 +1,156 @@
+﻿Imports Windows.Networking.BackgroundTransfer
+Imports Windows.Storage
+Imports Windows.System
+Imports Windows.UI
+Imports Windows.UI.StartScreen
+
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private Sub Page_Loading(sender As FrameworkElement, args As Object)
+
+        Dim recursos As Resources.ResourceLoader = New Resources.ResourceLoader()
+
+        tbDirectorioGOGJuego.Text = recursos.GetString("Directorio")
+        buttonAñadirGOGJuego.Content = recursos.GetString("Boton Busqueda")
+        checkboxTilesGOGTitulo.Content = recursos.GetString("Titulo Tile")
+        buttonVotaciones.Content = recursos.GetString("Boton Votar")
+        buttonContactar.Content = recursos.GetString("Boton Contactar")
+        buttonWeb.Content = recursos.GetString("Boton Web")
+
+        Listado.Generar(gridViewTilesGOG, buttonAñadirGOGJuego, progressRingCarga, scrollViewerGridGOG, False)
+
+    End Sub
+
+    '-----------------------------------------------------------------------------
+
+    Private Sub buttonAñadirGOGJuego_Click(sender As Object, e As RoutedEventArgs) Handles buttonAñadirGOGJuego.Click
+
+        Listado.Generar(gridViewTilesGOG, buttonAñadirGOGJuego, progressRingCarga, scrollViewerGridGOG, True)
+
+    End Sub
+
+    Private Sub buttonAñadirGOGJuego_PointerEntered(sender As Object, e As PointerRoutedEventArgs) Handles buttonAñadirGOGJuego.PointerEntered
+
+        buttonAñadirGOGJuego.BorderBrush = New SolidColorBrush(Colors.Black)
+        buttonAñadirGOGJuego.Background = New SolidColorBrush(Colors.DarkGray)
+
+    End Sub
+
+    Private Sub buttonAñadirGOGJuego_PointerExited(sender As Object, e As PointerRoutedEventArgs) Handles buttonAñadirGOGJuego.PointerExited
+
+        buttonAñadirGOGJuego.BorderBrush = New SolidColorBrush(Colors.DarkGray)
+        buttonAñadirGOGJuego.Background = New SolidColorBrush(Colors.Transparent)
+
+    End Sub
+
+    '-----------------------------------------------------------------------------
+
+    Private Sub buttonVotaciones_PointerEntered(sender As Object, e As PointerRoutedEventArgs) Handles buttonVotaciones.PointerEntered
+
+        buttonVotaciones.BorderBrush = New SolidColorBrush(Colors.Black)
+        buttonVotaciones.Background = New SolidColorBrush(Colors.DarkGray)
+
+    End Sub
+
+    Private Sub buttonVotaciones_PointerExited(sender As Object, e As PointerRoutedEventArgs) Handles buttonVotaciones.PointerExited
+
+        buttonVotaciones.BorderBrush = New SolidColorBrush(Colors.DarkGray)
+        buttonVotaciones.Background = New SolidColorBrush(Colors.Transparent)
+
+    End Sub
+
+    Private Async Sub buttonVotaciones_Click(sender As Object, e As RoutedEventArgs) Handles buttonVotaciones.Click
+
+        Await Launcher.LaunchUriAsync(New Uri("ms-windows-store:REVIEW?PFN=" + Package.Current.Id.FamilyName))
+
+    End Sub
+
+    '-----------------------------------------------------------------------------
+
+    Private Sub buttonContactar_PointerEntered(sender As Object, e As PointerRoutedEventArgs) Handles buttonContactar.PointerEntered
+
+        buttonContactar.BorderBrush = New SolidColorBrush(Colors.Black)
+        buttonContactar.Background = New SolidColorBrush(Colors.DarkGray)
+
+    End Sub
+
+    Private Sub buttonContactar_PointerExited(sender As Object, e As PointerRoutedEventArgs) Handles buttonContactar.PointerExited
+
+        buttonContactar.BorderBrush = New SolidColorBrush(Colors.DarkGray)
+        buttonContactar.Background = New SolidColorBrush(Colors.Transparent)
+
+    End Sub
+
+    Private Async Sub buttonContactar_Click(sender As Object, e As RoutedEventArgs) Handles buttonContactar.Click
+
+        Await Launcher.LaunchUriAsync(New Uri("https://pepeizqapps.com/contact/"))
+
+    End Sub
+
+    '-----------------------------------------------------------------------------
+
+    Private Sub buttonWeb_PointerEntered(sender As Object, e As PointerRoutedEventArgs) Handles buttonWeb.PointerEntered
+
+        buttonWeb.BorderBrush = New SolidColorBrush(Colors.Black)
+        buttonWeb.Background = New SolidColorBrush(Colors.DarkGray)
+
+    End Sub
+
+    Private Sub buttonWeb_PointerExited(sender As Object, e As PointerRoutedEventArgs) Handles buttonWeb.PointerExited
+
+        buttonWeb.BorderBrush = New SolidColorBrush(Colors.DarkGray)
+        buttonWeb.Background = New SolidColorBrush(Colors.Transparent)
+
+    End Sub
+
+    Private Async Sub buttonWeb_Click(sender As Object, e As RoutedEventArgs) Handles buttonWeb.Click
+
+        Await Launcher.LaunchUriAsync(New Uri("https://pepeizqapps.com/"))
+
+    End Sub
+
+    '-----------------------------------------------------------------------------
+
+    Private Sub checkboxTilesGOGTitulo_Checked(sender As Object, e As RoutedEventArgs) Handles checkboxTilesGOGTitulo.Checked
+
+        ApplicationData.Current.LocalSettings.Values("titulotilegog") = "on"
+
+    End Sub
+
+    Private Sub checkboxTilesGOGTitulo_Unchecked(sender As Object, e As RoutedEventArgs) Handles checkboxTilesGOGTitulo.Unchecked
+
+        ApplicationData.Current.LocalSettings.Values("titulotilegog") = "off"
+
+    End Sub
+
+    '-----------------------------------------------------------------------------
+
+    Private Async Sub gridviewTiles_ItemClick(sender As Object, e As ItemClickEventArgs) Handles gridViewTilesGOG.ItemClick
+
+        Dim tile As Tiles = e.ClickedItem
+
+        Dim ficheroImagen As StorageFile = Await ApplicationData.Current.LocalFolder.CreateFileAsync("headergog.png", CreationCollisionOption.GenerateUniqueName)
+        Dim downloader As BackgroundDownloader = New BackgroundDownloader()
+        Dim descarga As DownloadOperation = downloader.CreateDownload(tile.imagenUri, ficheroImagen)
+        Await descarga.StartAsync
+
+        Dim nuevaTile As SecondaryTile = New SecondaryTile(tile.id, tile.titulo, tile.enlace.AbsolutePath, New Uri("ms-appdata:///local/" + ficheroImagen.Name, UriKind.RelativeOrAbsolute), TileSize.Wide310x150)
+
+        Dim frame As FrameworkElement = TryCast(sender, FrameworkElement)
+        Dim button As GeneralTransform = frame.TransformToVisual(Nothing)
+        Dim point As Point = button.TransformPoint(New Point())
+        Dim rect As Rect = New Rect(point, New Size(frame.ActualWidth, frame.ActualHeight))
+
+        nuevaTile.RoamingEnabled = False
+        nuevaTile.VisualElements.Wide310x150Logo = New Uri("ms-appdata:///local/" + ficheroImagen.Name, UriKind.RelativeOrAbsolute)
+
+        If ApplicationData.Current.LocalSettings.Values("titulotilegog") = "on" Then
+            nuevaTile.VisualElements.ShowNameOnWide310x150Logo = True
+        End If
+
+        Await nuevaTile.RequestCreateForSelectionAsync(rect)
+
+    End Sub
+
+End Class
---
+++ GOG Tiles/Modulos/Listado.vb
@@ -0,0 +1,245 @@
+﻿Imports Windows.Storage
+Imports Windows.Storage.AccessCache
+Imports Windows.Storage.Pickers
+
+Module Listado
+
+    Dim clave As String = "carpetagog03"
+
+    Public Async Sub Generar(gridview As GridView, button As Button, pr As ProgressRing, sv As ScrollViewer, boolBuscarCarpeta As Boolean)
+
+        button.IsEnabled = False
+        pr.IsActive = True
+
+        Dim carpetas As ApplicationDataContainer = ApplicationData.Current.LocalSettings
+
+        Dim i As Integer = 0
+        If boolBuscarCarpeta = True Then
+            Try
+                Dim picker As FolderPicker = New FolderPicker()
+
+                picker.FileTypeFilter.Add("*")
+                picker.ViewMode = PickerViewMode.List
+
+                Dim carpeta As StorageFolder = Await picker.PickSingleFolderAsync()
+                Dim carpetaTemp As StorageFolder = Nothing
+
+                i = 0
+                While i < (carpetas.Values("numCarpetas") + 1)
+
+                    Try
+                        carpetaTemp = Await StorageApplicationPermissions.FutureAccessList.GetFolderAsync(clave + i.ToString)
+                    Catch ex As Exception
+                        StorageApplicationPermissions.FutureAccessList.AddOrReplace(clave + i.ToString, carpeta)
+                        carpetas.Values("numCarpetas") = i + 1
+                        Exit While
+                    End Try
+
+                    i += 1
+                End While
+
+            Catch ex As Exception
+
+            End Try
+        End If
+
+        '-------------------------------------------------------------
+
+        Dim listaFinal As List(Of Tiles) = New List(Of Tiles)
+
+        i = 0
+        If gridview.Items.Count > 0 Then
+            While i < gridview.Items.Count
+                Dim tile As Tiles = gridview.Items(i)
+
+                Dim tituloBool As Boolean = False
+                Dim g As Integer = 0
+                While g < listaFinal.Count
+                    If listaFinal(g).titulo = tile.titulo Then
+                        tituloBool = True
+                    End If
+                    g += 1
+                End While
+
+                If tituloBool = False Then
+                    listaFinal.Add(New Tiles(tile.titulo, tile.id, tile.enlace, tile.imagen, tile.imagenUri))
+                End If
+
+                i += 1
+            End While
+        End If
+
+        '-------------------------------------------------------------
+
+        i = 0
+        While i < carpetas.Values("numCarpetas") + 1
+            Dim carpeta As StorageFolder = Nothing
+
+            Try
+                carpeta = Await StorageApplicationPermissions.FutureAccessList.GetFolderAsync(clave + i.ToString)
+            Catch ex As Exception
+
+            End Try
+
+            If Not carpeta Is Nothing Then
+                Dim carpetasJuegos As IReadOnlyList(Of StorageFolder) = Await carpeta.GetFoldersAsync()
+
+                For Each carpetaJuego As StorageFolder In carpetasJuegos
+                    Dim ficheros As IReadOnlyList(Of StorageFile) = Await carpetaJuego.GetFilesAsync()
+
+                    For Each fichero As StorageFile In ficheros
+                        If fichero.DisplayName.Contains("goggame-") And fichero.FileType.Contains(".info") Then
+                            Dim texto As String = Await FileIO.ReadTextAsync(fichero)
+
+                            If Not texto = Nothing Then
+                                Dim temp, temp2, temp3 As String
+                                Dim int, int2, int3 As Integer
+
+                                int = texto.IndexOf(ChrW(34) + "gameId" + ChrW(34))
+                                temp = texto.Remove(0, int + 8)
+
+                                int2 = temp.IndexOf(ChrW(34))
+                                temp2 = temp.Remove(0, int2 + 1)
+
+                                int3 = temp2.IndexOf(ChrW(34))
+                                temp3 = temp2.Remove(int3, temp2.Length - int3)
+
+                                Dim id As String = temp3.Trim
+
+                                '---------------------------
+
+                                Dim temp4, temp5, temp6 As String
+                                Dim int4, int5, int6 As Integer
+
+                                int4 = texto.IndexOf(ChrW(34) + "name" + ChrW(34))
+                                temp4 = texto.Remove(0, int4 + 6)
+
+                                int5 = temp4.IndexOf(ChrW(34))
+                                temp5 = temp4.Remove(0, int5 + 1)
+
+                                int6 = temp5.IndexOf(ChrW(34))
+                                temp6 = temp5.Remove(int6, temp5.Length - int6)
+
+                                Dim titulo As String = temp6.Trim
+
+                                '---------------------------
+
+                                Dim temp7, temp8, temp9 As String
+                                Dim int7, int8, int9 As Integer
+
+                                int7 = texto.IndexOf(ChrW(34) + "path" + ChrW(34))
+                                temp7 = texto.Remove(0, int7 + 6)
+
+                                int8 = temp7.IndexOf(ChrW(34))
+                                temp8 = temp7.Remove(0, int8 + 1)
+
+                                int9 = temp8.IndexOf(ChrW(34))
+                                temp9 = temp8.Remove(int9, temp8.Length - int9)
+
+                                Dim acceso As String = Nothing
+
+                                If Not texto.Contains(ChrW(34) + "arguments" + ChrW(34)) Then
+                                    acceso = carpeta.Path + "\" + temp9.Trim
+                                Else
+                                    Dim temp10, temp11, temp12 As String
+                                    Dim int10, int11, int12 As Integer
+
+                                    int10 = texto.IndexOf(ChrW(34) + "arguments" + ChrW(34))
+                                    temp10 = texto.Remove(0, int10 + 9)
+
+                                    int11 = temp10.IndexOf(ChrW(34))
+                                    temp11 = temp10.Remove(0, int11 + 1)
+
+                                    int12 = temp11.IndexOf("}")
+                                    temp12 = temp11.Remove(int12, temp11.Length - int12)
+
+                                    temp12 = temp12.Trim
+                                    temp12 = temp12.Remove(temp12.Length - 1, 1)
+
+                                    acceso = carpeta.Path + "\" + temp9.Trim + temp12.Trim
+                                End If
+
+                                If Not acceso = Nothing Then
+                                    acceso = acceso.Replace("\\", "\")
+                                End If
+
+                                '---------------------------
+
+                                Dim temp14, temp15, temp16 As String
+                                Dim int14, int15, int16 As Integer
+
+                                int14 = texto.IndexOf(ChrW(34) + "link" + ChrW(34))
+                                temp14 = texto.Remove(0, int14 + 6)
+
+                                int15 = temp14.IndexOf(ChrW(34))
+                                temp15 = temp14.Remove(0, int15 + 1)
+
+                                int16 = temp15.IndexOf(ChrW(34))
+                                temp16 = temp15.Remove(int16, temp15.Length - int16)
+
+                                temp16 = temp16.Replace("/en/support/", "/game/")
+
+                                Dim html As String = Await Decompiladores.HttpClient(New Uri(temp16))
+
+                                Dim temp17, temp18, temp19 As String
+                                Dim int17, int18, int19 As Integer
+
+                                int17 = html.IndexOf(ChrW(34) + "og:image" + ChrW(34))
+                                temp17 = html.Remove(0, int17 + 5)
+
+                                int18 = temp17.IndexOf("content=")
+                                temp18 = temp17.Remove(0, int18 + 9)
+
+                                int19 = temp18.IndexOf(ChrW(34))
+                                temp19 = temp18.Remove(int19, temp18.Length - int19)
+
+                                If Not temp19 = Nothing Then
+                                    If Not temp19.Contains("http:") Then
+                                        temp19 = "http:" + temp19
+                                        temp19 = temp19.Replace(".jpg", "_340.jpg")
+                                    End If
+
+                                    Dim imagen As Uri = New Uri(temp19, UriKind.RelativeOrAbsolute)
+
+                                    Dim bitmap As New BitmapImage
+                                    bitmap.CreateOptions = BitmapCreateOptions.IgnoreImageCache
+                                    bitmap.UriSource = imagen
+
+                                    Dim tituloBool As Boolean = False
+                                    Dim g As Integer = 0
+                                    While g < listaFinal.Count
+                                        If listaFinal(g).titulo = titulo Then
+                                            tituloBool = True
+                                        End If
+                                        g += 1
+                                    End While
+
+                                    If tituloBool = False Then
+                                        listaFinal.Add(New Tiles(titulo, id, New Uri(acceso), bitmap, imagen))
+                                    End If
+                                End If
+                            End If
+                        End If
+                    Next
+                Next
+            End If
+            i += 1
+        End While
+
+        If listaFinal.Count > 0 Then
+            listaFinal.Sort(Function(x, y) x.titulo.CompareTo(y.titulo))
+            sv.Visibility = Visibility.Visible
+        Else
+            If boolBuscarCarpeta = True Then
+                Dim recursos As Resources.ResourceLoader = New Resources.ResourceLoader()
+                MessageBox.EnseñarMensaje(recursos.GetString("Fallo1"))
+            End If
+        End If
+
+        gridview.ItemsSource = listaFinal
+
+        button.IsEnabled = True
+        pr.IsActive = False
+
+    End Sub
+End Module
